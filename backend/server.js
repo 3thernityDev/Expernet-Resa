07443923d464366d83d4f4743d7d4f8783f1631f
3thernitydev <3thernitydev@gmail.com>
2025-10-14 10:55:49 +0400

    feat(server): ajout de cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { initDB } from "./config/db.js";
 import reservationRoutes from "./routes/roomRoutes.js";
 
 dotenv.config();
 const app = express();
+
+// Autoriser CORS depuis le frontend local
+app.use(
+    cors({
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    })
+);
 
 app.use(express.json());
 app.use("/api/reservations", reservationRoutes);
